9b9e6fbd36580f66559ca404b9ef2a50619736fd
dave936 <50265779+dave936@users.noreply.github.com>
2020-02-03 15:22:46 +0000

    Added an unfinished textbox function
    For creating a profile.

--- Modules/functions.py
+++ Modules/functions.py
@@ -62,3 +62,39 @@ def profileMenu(profile1, profile2):
             pygame.display.flip()
 def songMenu():
     pass
+ 
+ 
+ def textbox():
+    DISPLAY.fill(CYAN)
+    back_button = button(0, 550, 175, 50, "Cancel...")
+    back_button.draw()
+    rect = pygame.draw.rect(DISPLAY, GREY, (200, 300, 300, 50))
+    font = pygame.font.SysFont("courier new", 40)
+    usr_string = ""
+    while True:
+        for event in pygame.event.get():
+            #Rendering the cancel button
+            back_button.update(event)
+            if back_button.ButtonClicked == True:
+                return False
+            #Checking if quit attempt has been made.
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            #Checking if a key was pressed down.
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    usr_string = usr_string[:-1] #Removes the last character from string
+                    DISPLAY.fill(GREY, (rect.x, rect.y, rect.width, rect.height)) #Fills textbox modified string can render over
+                elif event.key == 13:
+                    if len(usr_string) > 15:
+                        print("Username should be less than 15 characters long!")
+                    else:
+                        return usr_string #usr_string returned to function to commit to database
+                elif event.key >= 97 and event.key <= 122: #A-Z keys
+                    usr_string += event.unicode
+                elif event.key >= 48 and event.key <= 57: #0-9 keys
+                    usr_string += str(event.unicode)
+            txt = font.render(usr_string, True, TEXT_COLOUR)
+            DISPLAY.blit(txt, (rect.x + 5, rect.y + 5))
+            pygame.display.flip()
