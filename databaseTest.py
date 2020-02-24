9e56b7b55a2a2867b5b79f858d5853b54936f2f7
dave936 <50265779+dave936@users.noreply.github.com>
2020-02-24 13:18:58 +0000

    Add previously used queries

--- databaseTest.py
+++ databaseTest.py
@@ -4,6 +4,9 @@ c = conn.cursor() #Cursor object allows you to interact with the database.
 
 c.execute("CREATE TABLE profiles (ProfileID, UserName, SongsPlayed, TimePlayed)") #SQL Statement to create table.
 c.execute("INSERT INTO profiles VALUES (0, 'example', 0, 23354)") #Add example entry to test.
-c.execute(
+
+query = "ALTER TABLE profiles ADD COLUMN Level"
+#(ProfileID, UserName, SongsPlayed, TimePlayed, Level)
+c.execute("DELETE FROM profiles WHERE UserName='False'")
 conn.commit()
 conn.close()
